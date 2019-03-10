8b48a32efd178ab0d9c428c9deead067e3b8ff2b
Weatherly02 <47132416+Weatherly02@users.noreply.github.com>
2019-03-10 12:54:58 -0500

    keep on mapping

--- src/edu/dmacc/codedsm/hw10/CardDeck.java
+++ src/edu/dmacc/codedsm/hw10/CardDeck.java
@@ -1,19 +1,16 @@
 package edu.dmacc.codedsm.hw10;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.util.*;
+import java.util.ArrayList;
 
 public class CardDeck {
 
     public static void main(String[] args) {
-        Map<Object, String> suits = new HashMap<Object, String>();
-        Map<String, List<Integer>> rankValue = new HashMap<String, List<Integer>>();
+        Map<String, List> cards = new HashMap<String, List>();
 
-        suits.put(0, "Hearts");
-        suits.put(1, "Diamands");
-        suits.put(2, "Spades");
-        suits.put(3, "Clubs");
-
-        List<Integer> rank = new ArrayList<Integer>();
+        ArrayList<Integer> rank = new ArrayList<Integer>();
         rank.add(1);
         rank.add(2);
         rank.add(3);
@@ -23,37 +20,28 @@ public class CardDeck {
         rank.add(7);
         rank.add(8);
         rank.add(9);
+        rank.add(10);
         rank.add(11);
         rank.add(12);
         rank.add(13);
 
-        //rankValue.put("int list", rank);  // shows [1,2,3...13]
-        //System.out.println(rank.get(8));  // shows 9
-
-        //rank = rankValue.get("int list");
-        //System.out.println(rank);
-
-        System.out.println(suits);
-        //System.out.println(suits.get(2));  //shows Spades
-
-        // for (suits : suits) {
-        for (int i = 0; i < rank.size(); i++) {
-            //System.out.println(i);
-            // System.out.println(i + "value" + String.valueOf(rank.get(i)));
-
-            System.out.println(String.valueOf(rank.get(i)));
-
-            for (int b = 0; b < 4; b++) {
-                System.out.println(String.valueOf(suits.get(b)));
+        cards.put("Hearts", rank);
+        cards.put("Diamonds", rank);
+        cards.put("Spades", rank);
+        cards.put("Clubs", rank);
 
 
-                //for (String suit : suit.get(b)) {
-                //  for (Integer integer : rank) {
-                //System.out.println("Suit " + getsuit + " - " + "Value " + integer);
+        for (Map.Entry<String, List> entry : cards.entrySet()) {
+            for (int b = 1; b <= 13; b++) {
+                System.out.println(entry.getKey() + " - " + b);
             }
 
         }
-
     }
 }
+
+
+
+
+
 
---
+++ src/edu/dmacc/codedsm/hw10/Suit.java
@@ -0,0 +1,11 @@
+package edu.dmacc.codedsm.hw10;
+
+public class Suit {
+    public static void main(String[] args) {
+        String suitOne = "Hearts";
+        String suitTwo = "Diamonds";
+        String suitThree = "Spades";
+        String suitFour = "Clubs";
+
+    }
+}
